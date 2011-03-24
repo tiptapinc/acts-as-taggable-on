25a3308dfe39a89d29306fa82554d5b7ab59dd4f
Ben Woosley <ben.woosley@gmail.com>
2011-03-23 23:51:16 -0700

    Use proper scopes and class << self in tag.  Replace runtime connection checking for LIKE vs ILIKE with an overridable method, Tag.like_operator

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -7,51 +7,44 @@ module ActsAsTaggableOn
     has_many :taggings, :dependent => :destroy, :class_name => 'ActsAsTaggableOn::Tagging'
 
     ### VALIDATIONS:
-
     validates_presence_of :name
     validates_uniqueness_of :name
 
     ### SCOPES:
+    # Override this with 'ILIKE' for case-insensitive operation on postgres
+    cattr_accessor :like_operator
+    self.like_operator = 'LIKE'
 
-    def self.using_postgresql?
-      connection.adapter_name == 'PostgreSQL'
-    end
-
-    def self.named(name)
-      where(["name #{like_operator} ?", name])
-    end
-
-    def self.named_any(list)
-      where(list.map { |tag| sanitize_sql(["name #{like_operator} ?", tag.to_s]) }.join(" OR "))
-    end
-
-    def self.named_like(name)
-      where(["name #{like_operator} ?", "%#{name}%"])
-    end
-
-    def self.named_like_any(list)
-      where(list.map { |tag| sanitize_sql(["name #{like_operator} ?", "%#{tag.to_s}%"]) }.join(" OR "))
-    end
+    scope :named, lambda {|name| where(["name #{like_operator} ?", name]) }
+    scope :named_any, lambda {|list| where(list.map { |tag| sanitize_sql(["name #{like_operator} ?", tag.to_s]) }.join(" OR ")) }
+    scope :named_like, lambda {|name| where(["name #{like_operator} ?", "%#{name}%"]) }
+    scope :named_like_any, lambda {|list| where(list.map { |tag| sanitize_sql(["name #{like_operator} ?", "%#{tag.to_s}%"]) }.join(" OR ")) }
 
     ### CLASS METHODS:
 
-    def self.find_or_create_with_like_by_name(name)
-      named_like(name).first || create(:name => name)
-    end
+    class << self
+      def using_postgresql?
+        connection.adapter_name == 'PostgreSQL'
+      end
 
-    def self.find_or_create_all_with_like_by_name(*list)
-      list = [list].flatten
+      def find_or_create_with_like_by_name(name)
+        named_like(name).first || create(:name => name)
+      end
 
-      return [] if list.empty?
+      def find_or_create_all_with_like_by_name(*list)
+        list = [list].flatten
 
-      existing_tags = Tag.named_any(list)
-      new_tag_names = list.reject do |name|
-                        name = comparable_name(name)
-                        existing_tags.any? { |tag| comparable_name(tag.name) == name }
-                      end
-      created_tags  = new_tag_names.map { |name| Tag.create(:name => name) }
+        return [] if list.empty?
 
-      existing_tags + created_tags
+        existing_tags = Tag.named_any(list)
+        new_tag_names = list.reject do |name|
+                          name = comparable_name(name)
+                          existing_tags.any? { |tag| comparable_name(tag.name) == name }
+                        end
+        created_tags  = new_tag_names.map { |name| Tag.create(:name => name) }
+
+        existing_tags + created_tags
+      end
     end
 
     ### INSTANCE METHODS:
@@ -70,10 +63,6 @@ module ActsAsTaggableOn
 
     class << self
       private
-        def like_operator
-          using_postgresql? ? 'ILIKE' : 'LIKE'
-        end
-
         def comparable_name(str)
           RUBY_VERSION >= "1.9" ? str.downcase : str.mb_chars.downcase
         end
