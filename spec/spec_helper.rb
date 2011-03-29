75b1d9463d3779f4d40c412a5473993d1f014d00
Ben Woosley <ben.woosley@gmail.com>
2011-03-28 22:53:12 -0700

    No need for if/else when else raises

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,21 +32,21 @@ end
 ENV['DB'] ||= 'sqlite3'
 
 database_yml = File.expand_path('../database.yml', __FILE__)
-if File.exists?(database_yml)
-  active_record_configuration = YAML.load_file(database_yml)[ENV['DB']]
-
-  ActiveRecord::Base.establish_connection(active_record_configuration)
-  ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
-
-  ActiveRecord::Base.silence do
-    ActiveRecord::Migration.verbose = false
-
-    load(File.dirname(__FILE__) + '/schema.rb')
-    load(File.dirname(__FILE__) + '/models.rb')
-  end
-
-else
+if !File.exists?(database_yml)
   raise "Please create #{database_yml} first to configure your database. Take a look at: #{database_yml}.sample"
+end
+
+active_record_configuration = YAML.load_file(database_yml)[ENV['DB']]
+
+require 'active_support/core_ext/logger'
+ActiveRecord::Base.establish_connection(active_record_configuration)
+ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
+
+ActiveRecord::Base.silence do
+  ActiveRecord::Migration.verbose = false
+
+  load(File.dirname(__FILE__) + '/schema.rb')
+  load(File.dirname(__FILE__) + '/models.rb')
 end
 
 def clean_database!
